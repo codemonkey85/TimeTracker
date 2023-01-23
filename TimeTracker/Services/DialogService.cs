c6c76c2d70644fd023888f58d8a7d23334b4f37b
Michael Bond <michael@bondcodes.com>
2023-01-23 01:25:02 -0500

    Use bind:after, cleanup

--- TimeTracker/Services/DialogService.cs
+++ TimeTracker/Services/DialogService.cs
@@ -2,13 +2,9 @@
 
 public record DialogService(IJSRuntime JsRuntime) : IDialogService
 {
-    public async Task<bool> ConfirmAsync(string title, string message, string accept, string cancel)
-    {
-        return await JsRuntime.ConfirmAsync(message);
-    }
+    public async Task<bool> ConfirmAsync(string title, string message, string accept, string cancel) =>
+        await JsRuntime.ConfirmAsync(message);
 
-    public async Task AlertAsync(string title, string message, string cancel)
-    {
+    public async Task AlertAsync(string title, string message, string cancel) =>
         await JsRuntime.AlertAsync(message);
-    }
 }
