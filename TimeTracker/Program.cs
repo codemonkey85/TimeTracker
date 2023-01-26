eb7038200fa803f30274e8b1f5ce564e05030c00
Michael Bond <michael@bondcodes.com>
2023-01-25 21:18:15 -0500

    Fix logging

--- TimeTracker/Program.cs
+++ TimeTracker/Program.cs
@@ -3,6 +3,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services
+    .AddLogging()
     .AddScoped<TimeTrackerJs>()
     .AddScoped<IndexedDbAccessor>()
     .AddScoped<IDataService, DataService>()
